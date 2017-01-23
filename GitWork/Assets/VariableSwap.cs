b165e9d68ee6e3a5d8cff6e2afc753ebac66a489
LEARNET\jamie.richman <jamie.r@hotmail.se>
2017-01-23 12:58:43 +0100

    Fixed Problems and Errors, Finished Mission Nr 1

--- GitWork/Assets/VariableSwap.cs
+++ GitWork/Assets/VariableSwap.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class VariableSwap : MonoBehaviour {
 
-    int a;
-    int b;
+    int a=1;
+    int b=2;
     int c;
 
 	void Start () 
     {
+        Debug.Log(a);
+        Debug.Log(b);
         c = b;
         b = a;
         a = c;
+        Debug.Log(a);
+        Debug.Log(b);
 	}
 	
 	// Update is called once per frame
