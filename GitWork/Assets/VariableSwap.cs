2aebe3c6eb68088a6b5f8f79922264c353953e74
LEARNET\jamie.richman <jamie.r@hotmail.se>
2017-01-23 12:23:30 +0100

    Started project and Added first lines of code

---
+++ GitWork/Assets/VariableSwap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariableSwap : MonoBehaviour {
+
+    int a;
+    int b;
+    int c;
+
+	void Start () 
+    {
+        c = b;
+        b = a;
+        a = c;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
