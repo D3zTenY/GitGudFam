69846958ab79252dffd7936e2baf5d991b18a91f
LEARNET\jamie.richman <jamie.r@hotmail.se>
2017-01-30 12:57:53 +0100

    Compared three values to make sure that there is one in the middle

--- GitWork/Assets/IfVariable.cs
+++ GitWork/Assets/IfVariable.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IfVariable : MonoBehaviour {
+public class IfVariable : MonoBehaviour
+{
+    //The values of a, b and c
+    public int a = 5;
+    public int b = 10;
+    public int c = 15;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    //Checks the position for a in cases of c and b
+    {
+        if (a < b && a < c || a > b && a < b)
+        //Prints a if the requirements are met
+        {
+            print("a");
+        }
+        //Checks the position of b in cases of a and c
+        else if (b > a && b < c || b < a && b > c)
+        //Prints b if the requirements are met 
+        {
+            print("b");
+        }
+        //Prints c if none of the requirements above are met
+        else
+        {
+            print("c");
+        }
+
+
+    }
+
+        // Update is called once per frame
+        void Update () {
+        }
 }
