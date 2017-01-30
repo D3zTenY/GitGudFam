388a84335978017af31bc3bbb5c28faf73bed23f
LEARNET\jamie.richman <jamie.r@hotmail.se>
2017-01-30 11:14:15 +0100

    Compared two values and printed if one was bigger than the other.

---
+++ GitWork/Assets/VariableCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableCount : MonoBehaviour {
+
+    // The changable values of a and b
+    public int a = 10;
+    public int b = 20;
+
+	// Use this for initialization
+	void Start () {
+        if (a > b)
+    // Prints if a has a higher value
+        {
+            print("a is bigger");
+        }
+        else
+        //Prints if b has a higher value
+        {
+            print("b is Bigger");
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
