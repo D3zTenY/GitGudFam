4f921b46bbd2fa34776c36970258f87f708e46f9
LEARNET\jamie.richman <jamie.r@hotmail.se>
2017-01-30 11:24:32 +0100

    Combines two variables and prints the result as c.

--- GitWork/Assets/VariableConvertion.cs
+++ GitWork/Assets/VariableConvertion.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VariableConvertion : MonoBehaviour {
+    //Values of a, b and c
+    public int a = 64;
+    public int b = 62;
+    int c = 0;
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        //Combines a + b to become c
+        c = a + b;
+        //Prints the value of c
+        Debug.Log(c);
+        //Prints the values combined
+        Debug.Log(a + b);
 		
 	}
 	
